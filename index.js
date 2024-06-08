36944995b970f9ab10a2946b1f56d464f3a04456
JeysRe <156866774+JeysRe@users.noreply.github.com>
2024-06-08 21:16:29 +0000

    Agrega archivos cajahuancayo.txt, index.js y package.json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const readline = require('readline');
 const fs = require('fs');
 
-const rl = readline.createInterface({
+const rl = readline.createInterface({                         
   input: fs.createReadStream('cajahuancayo.txt'), 
   crlfDelay: Infinity
 });
